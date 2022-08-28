495138ff8f64b6cc81fe1d13d0036ceae8f24a28
KayAhn0126 <dkseotn123@gmail.com>
2022-08-28 20:47:54 +0900

    refactor: 명상컨텐츠리스트 구현

--- MeditationContentsList/MeditationContentsList/MeditationCollectionViewCell.swift
+++ MeditationContentsList/MeditationContentsList/MeditationCollectionViewCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class MeditationCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.backgroundColor = UIColor.systemIndigo
+        contentView.layer.cornerRadius = 10
+    }
+    func configure(_ meditation: Meditation) {
+        titleLabel.text = meditation.title
+        descriptionLabel.text = meditation.description
+        weatherImageView.image = UIImage(systemName: meditation.imageName)?.withRenderingMode(.alwaysOriginal)
+    }
 }
--- MeditationContentsList/MeditationContentsList/ViewController.swift
+++ MeditationContentsList/MeditationContentsList/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Section {
+    case main
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var changeModeButton: UIButton!
+    
+    var curated: Bool = false
+    var items: [Meditation] = Meditation.list
+    
+    typealias Item = Meditation
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeditationCollectionViewCell", for: indexPath) as? MeditationCollectionViewCell else {
+                return nil
+            }
+            cell.configure(item)
+            return cell
+        })
+        
+        // Snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+        
+        let title = curated ? "See All" : "See Recommendation"
+        changeModeButton.setTitle(title, for: .normal)
+        changeModeButton.layer.cornerRadius = 10
     }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
 
-
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        section.interGroupSpacing = 10
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    @IBAction func changeModeTapped(_ sender: UIButton) {
+        curated.toggle()
+        self.items = curated ? Meditation.recommendations : Meditation.list
+        var snapshot =  NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        let title = curated ? "See All" : "See Recommendation"
+        changeModeButton.setTitle(title, for: .normal)
+    }
 }
 
