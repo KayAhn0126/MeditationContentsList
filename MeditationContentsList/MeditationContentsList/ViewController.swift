816ad4ac231d44a1b2acdada28febde117bb00d3
KayAhn0126 <dkseotn123@gmail.com>
2022-08-28 19:21:20 +0900

    feat: Meditation Contents List 프로젝트 추가

---
+++ MeditationContentsList/MeditationContentsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeditationContentsList
+//
+//  Created by Kay on 2022/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
